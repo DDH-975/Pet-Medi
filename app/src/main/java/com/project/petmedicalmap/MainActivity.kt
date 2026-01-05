caae79b8a194aff63242ebd94e9b804f060b795c
junho <junho5508@naver.com>
2026-01-05 21:05:29 +0900

    refactor: 불필요한 import 제거
    `MainActivity.kt` 파일에서 사용되지 않는 `android.widget.Toast` import 문을 삭제하여 코드를 정리했습니다.

--- app/src/main/java/com/project/petmedicalmap/MainActivity.kt
+++ app/src/main/java/com/project/petmedicalmap/MainActivity.kt
@@ -2,7 +2,6 @@ package com.project.petmedicalmap
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
